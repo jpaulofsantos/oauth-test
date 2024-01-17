4d47801ce0018845827a2704b4180173177c8607
JoaoPaulo <jpaulofsantos@gmail.com>
2024-01-16 22:49:13 -0300

    add user service

---
+++ src/main/java/com/devsuperior/demo/projections/UserDetailsProjection.java
@@ -0,0 +1,13 @@
+package com.devsuperior.demo.projections;
+
+public interface UserDetailsProjection {
+
+    String getUserName();
+
+    String getPassword();
+
+    Long getRoleId();
+
+    String getAuthority();
+
+}
